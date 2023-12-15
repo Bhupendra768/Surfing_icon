454f5d70e1d74549bab8bffbea6cf75c347c54ce
Bhupendra kumar <kumarbhupendra768@gmail.com>
2023-12-15 13:59:10 +0530

    new files added

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.getElementById('button');
+let surf = document.getElementsByClassName('circle');
+var i;
+function Circle_round(surf){
+   console.log("Happy journey")
+   surf.style.transform = `rotate(${--i}deg)`
+}
